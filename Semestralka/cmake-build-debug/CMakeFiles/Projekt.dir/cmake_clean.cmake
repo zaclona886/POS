6106356694d59be457b47d8ee5bf6e67d151ff3a
Pavol Petrovič <palko.petrovic@gmail.com>
2022-01-02 14:57:31 +0100

    Vytvorenie projektu

---
+++ Semestralka/cmake-build-debug/CMakeFiles/Projekt.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Projekt.dir/main.c.o"
+  "Projekt"
+  "Projekt.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Projekt.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
